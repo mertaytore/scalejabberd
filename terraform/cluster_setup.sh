69efad30f4d50d40458647066f1a5ad223c4c8e4
mertaytore <aytoremert@gmail.com>
2017-04-09 22:12:13 +0300

    unnecessary logging, out

--- terraform/cluster_setup.sh
+++ terraform/cluster_setup.sh
@@ -1,16 +1,19 @@
-echo 'locale setup start'
+#!/bin/bash
+
 export LANGUAGE=en_US:en
 export LC_ALL=en_US.UTF-8
 sudo locale-gen en_US.UTF-8
-echo 'locale setup done'
 
+# Current node's ip is stored in temp_ip variable to be compared with cluster_ip
 export temp_ip=$2
 export temp_ip=`echo ip-$temp_ip | tr  '.'  '-'`
 
+# Master node's ip is formatted and kept in ejabberd_cluster_ip, ready to be used
+# for join operation
 export cluster_ip=$1
 export cluster_ip=`echo ip-$cluster_ip | tr  '.'  '-'`
-
 export ejabberd_cluster_ip=`echo ejabberd@$cluster_ip | tr  '.'  '-'`
 
+# joins the node to the cluster. won't do the join operation if the 'master' node
+# (the node that has the cluster_ip) requests to join itself
 if [ $cluster_ip != $temp_ip ]; then sudo /home/ubuntu/ejabberd-17.01/bin/ejabberdctl join_cluster $ejabberd_cluster_ip; else echo 'cannot join itself'; fi
-#sudo /home/ubuntu/ejabberd-17.01/bin/ejabberdctl list_cluster
--- terraform/new_terra_setup.sh
+++ terraform/new_terra_setup.sh
@@ -4,32 +4,25 @@ export LANGUAGE=en_US:en
 export LC_ALL=en_US.UTF-8
 sudo locale-gen en_US.UTF-8
 
-echo 'ejabberd 17.01 installation'
+# download and install
 wget https://www.process-one.net/downloads/ejabberd/17.01/ejabberd-17.01-linux-x86_64-installer.run
 sudo chmod +x ejabberd-17.01-linux-x86_64-installer.run
 export temp_ip=$4
 export temp_ip=`echo ip-$temp_ip | tr  '.'  '-'`
 ./ejabberd-17.01-linux-x86_64-installer.run --mode unattended --installdir /home/ubuntu/ejabberd-17.01 --hostname $temp_ip --admin $1 --adminpw $2 --cluster 1 --ejabberddomain localhost
-sleep 5
-echo 'ejabberd 17.01 installation done'
 
-echo 'erlang cookie setup'
+# .erlang.cookie setup
 export cookie=$3
 sudo chown root /home/ejabberd/
 sudo chmod 660 /home/ejabberd/.erlang.cookie
 sudo -u ejabberd sh -c "echo ${3} > /home/ejabberd/.erlang.cookie"
 sudo chmod 400 /home/ejabberd/.erlang.cookie
 sudo chown ejabberd /home/ejabberd/
-echo 'erlang cookie changed'
 sudo cat /home/ejabberd/.erlang.cookie
 
-echo 'Starting ejabberd server'
+#echo 'Starting ejabberd server'
+# setup done, starting the ejabberd server
 sudo /home/ubuntu/ejabberd-17.01/bin/ejabberdctl start
 export cluster_ip=$5
 export cluster_ip=`echo ip-$cluster_ip | tr  '.'  '-'`
 export ejabberd_cluster_ip=`echo ejabberd@$cluster_ip | tr  '.'  '-'`
-#echo 'main cluster:'
-#echo $cluster_ip
-#echo 'ejabberd main cluster:'
-#echo $ejabberd_cluster_ip
-echo 'done & out'
